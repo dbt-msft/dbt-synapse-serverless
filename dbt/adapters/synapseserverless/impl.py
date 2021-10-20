953bbefc8067a1b787bca72efd1bede4744da138
swanderz <swanson.anders@gmail.com>
2021-10-20 16:06:21 -0700

    override BaseRelation class

--- dbt/adapters/synapseserverless/impl.py
+++ dbt/adapters/synapseserverless/impl.py
@@ -1,7 +1,9 @@
 from dbt.adapters.synapse import SynapseAdapter
 from dbt.adapters.synapseserverless import SynapseServerlessConnectionManager
+from dbt.adapters.synapseserverless.relation import SynapseServerlessRelation
 
 
 
 class SynapseServerlessAdapter(SynapseAdapter):
+    Relation = SynapseServerlessRelation
     ConnectionManager = SynapseServerlessConnectionManager
